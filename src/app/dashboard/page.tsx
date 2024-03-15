b894e71656cb84beea2206be91952553bb76e80f
MakoskiVictor <makoski.ed@gmail.com>
2024-03-15 10:43:18 -0300

    add styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard () {
     return(
-        <main>
-            <div>Dashboard</div>
-        </main>
+        <section className='h-[calc(100vh-7rem)] flex justify-center items-center '>
+            <div className="text-4xl text-slate-400 md:text-8xl " >Dashboard</div>
+        </section>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=' h-[calc(100vh-7rem)] flex justify-center items-center '>
-      <div className=" text-slate-500 text-8xl " >Home</div>
-    </main>
+    <section className=' h-[calc(100vh-7rem)] flex justify-center items-center '>
+      <div className="text-4xl text-slate-400 md:text-8xl " >Home</div>
+    </section>
   );
 }
