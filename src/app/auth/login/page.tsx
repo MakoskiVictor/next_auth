062576a4f087bf01a0ec6685c6d895a595cdbd7a
MakoskiVictor <makoski.ed@gmail.com>
2024-03-15 10:35:42 -0300

    Add signIn and notify

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,9 @@
 "use client"
 import { useForm } from "react-hook-form"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 type FormValues = {
     email: string
@@ -7,17 +11,34 @@ type FormValues = {
 }
 
 export default function LoginPage () {
+    // Avisos
+    const notify = () => toast("User not found!");
 
     const {register, formState: {errors}, handleSubmit} = useForm<FormValues>()
 
-    const onSubmit = handleSubmit(data => {
-        console.log(data)
+    const router = useRouter()
+
+    const onSubmit = handleSubmit(async (data) => {
+
+        const response = await signIn("credentials", {
+            email: data.email,
+            password: data.password,
+            redirect: false
+        })
+
+        if(response?.error) {
+            notify()
+        } else {
+            router.push("/dashboard")
+        }
+
     })
 
     return(
         <section className=' h-[calc(100vh-7rem)] flex justify-center items-center '>
             <form onSubmit={onSubmit} className='w-full sm:w-1/4 p-7 ' noValidate >
                 <h1 className=' text-slate-200 font-bold text-4xl mb-4 ' >LogIn</h1>
+                <ToastContainer theme='dark' />
 
                 <label htmlFor="email" className=' text-slate-500 mb-2 block text-sm ' > Email </label>
                 <input type="email"
