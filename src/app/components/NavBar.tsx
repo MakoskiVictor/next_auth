34f96118bc4ca939799bc8dbb55e0c855ebf0932
MakoskiVictor <makoski.ed@gmail.com>
2024-03-18 16:40:38 -0300

    add styles

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,13 +1,32 @@
 import Link from "next/link"
+import {getServerSession} from "next-auth/next"
+import {authOptions} from "@/app/api/auth/[...nextauth]/route"
 
-export function NavBar () {
+
+export async function NavBar () {
+
+    const session = await getServerSession(authOptions)
+
     return(
-        <nav className=" flex space-x-10 " >
-            <h2 className="text-slate-300 ">NextRegister</h2>
-            <ul className="text-slate-300 ">
-                <li><Link href="/auth/login" >LogIn</Link></li>
-                <li><Link href="/auth/register">Register</Link></li>
-                <li></li>
+        <nav className="flex justify-between items-center bg-gray-950 text-white px-24 py-3">
+            <h2 className="text-xl font-bold"><Link href="/">NextGatekeeper</Link></h2>
+
+            <ul className="flex gap-x-2">
+                {
+                    !session?.user ? (
+                        <>
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href="/auth/login" >LogIn</Link></li>
+                            <li><Link href="/auth/register">Register</Link></li>
+                        </>
+                    ) :
+                    (
+                        <>
+                        <li><Link href="/dashboard" >Dashboard</Link></li>
+                        <li><Link href="/api/auth/signout">Sign out</Link></li>
+                        </>
+                    )
+                }
             </ul>
         </nav>
     )
